d574154baf803472d1af19a935062038e5452f72
Douglas Henrique <douglas10_dodo@hotmail.com>
2025-09-04 14:51:17 -0300

    Obtendo post por Id

---
+++ src/main/java/com/douglashenrique/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.douglashenrique.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douglashenrique.workshopmongo.domain.Post;
+import com.douglashenrique.workshopmongo.repository.PostRepository;
+import com.douglashenrique.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) { 
+		 Optional<Post> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+		}
+}
+
